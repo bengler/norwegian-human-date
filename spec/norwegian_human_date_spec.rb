5f3db5c652dc93daa7bb5a261d73d14c2097bf53
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2012-11-22 11:30:33 +0100

    Update spec

--- spec/norwegian_human_date_spec.rb
+++ spec/norwegian_human_date_spec.rb
@@ -1,7 +1,18 @@
+# encoding: utf-8
 describe NorwegianHumanDate do
   describe ".relative" do
-    it 'parses time' do
+
+    it 'translates Time.now into "i dag"' do
       NorwegianHumanDate.relative(Time.now).should eq "i dag"
     end
+
+    it 'translates Time.now-1.day into "i går"' do
+      NorwegianHumanDate.relative(Time.now-1.day).should eq "i går"
+    end
+
+    it 'translates Time.now+1.day into "i morgen"' do
+      NorwegianHumanDate.relative(Time.now+1.day).should eq "i morgen"
+    end
+
   end
 end
