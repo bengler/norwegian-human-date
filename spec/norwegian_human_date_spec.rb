bb7612a358dfd9643243d199b003664351875b3c
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2012-11-22 11:37:01 +0100

    Add timecop and more specs

--- spec/norwegian_human_date_spec.rb
+++ spec/norwegian_human_date_spec.rb
@@ -1,17 +1,37 @@
 # encoding: utf-8
+require "timecop"
 describe NorwegianHumanDate do
   describe ".relative" do
 
-    it 'translates Time.now into "i dag"' do
-      NorwegianHumanDate.relative(Time.now).should eq "i dag"
-    end
+    Timecop.freeze(Time.at(1353578081)) do
+      it 'translates Time.now into "i dag"' do
+        NorwegianHumanDate.relative(Time.now).should eq "i dag"
+      end
 
-    it 'translates Time.now-1.day into "i går"' do
-      NorwegianHumanDate.relative(Time.now-1.day).should eq "i går"
-    end
+      it 'translates Time.now-1.day into "i går"' do
+        NorwegianHumanDate.relative(Time.now-1.day).should eq "i går"
+      end
 
-    it 'translates Time.now+1.day into "i morgen"' do
-      NorwegianHumanDate.relative(Time.now+1.day).should eq "i morgen"
+      it 'translates Time.now+1.day into "i morgen"' do
+        NorwegianHumanDate.relative(Time.now+1.day).should eq "i morgen"
+      end
+
+      it 'translates Time.now+1.day into "i morgen"' do
+        NorwegianHumanDate.relative(Time.now+1.day).should eq "i morgen"
+      end
+
+      it 'translates Time.now+2.days into "lørdag"' do
+        NorwegianHumanDate.relative(Time.now+2.days).should eq "lørdag"
+      end
+
+      it 'translates Time.now-2.days into "sist tirsdag"' do
+        NorwegianHumanDate.relative(Time.now-2.days).should eq "sist tirsdag"
+      end
+
+      it 'translates Time.now-1.week into "torsdag 15. november"' do
+        NorwegianHumanDate.relative(Time.now-1.week).should eq "torsdag 15. november"
+      end
+
     end
 
   end
